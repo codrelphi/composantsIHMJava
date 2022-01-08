1a72cd88f944a3a4f991f8ae7099984ffff72132
codrelphi <chancerel.codjovi@gmail.com>
2022-01-08 23:48:30 +0100

    code pour utiliser un éditeur de texte simple

---
+++ TestEditeurJava.java
@@ -0,0 +1,9 @@
+public class TestEditeurJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("Bonjour M. Nordine et Mme Meriema !");
+	}
+}
+
+// console: java TestEditeurJava.java
